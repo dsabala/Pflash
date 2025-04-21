2bd50ebbf1bdc6ca3c52b2f2d7e2843db8f11667
Dariusz Sabała <70911794+dsabala@users.noreply.github.com>
2025-04-21 14:14:01 +0200

    Add logging

--- src/pflash/cli.py
+++ src/pflash/cli.py
@@ -1,8 +1,19 @@
+import os
+import sys
+from loguru import logger
 import click
+
+LOG_DIR = os.path.expanduser("~/.config/pflash")
+os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE = os.path.join(LOG_DIR, "log.txt")
+logger.remove()
+logger.add(LOG_FILE, rotation="10 MB", retention="7 days", level="DEBUG", format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}")
+logger.add(sys.stdout, level="INFO", format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> {message}")
 
 @click.group()
 def cli_entrypoint():
     """pflash - Phoenix RTOS flash utility"""
+    logger.debug("Pflash CLI started")
     pass
 
 @cli_entrypoint.command()
@@ -10,4 +21,4 @@ def cli_entrypoint():
 @click.option("-c", "--config", required=True, type=str)
 def flash_via_ramdisk(serial, config):
     """Flash board using plo RAMDISK, debugger and console"""
-    click.echo(f"Flash via RAMDISK, serial = {serial}, config = {config}")
+    logger.info(f"Flash via RAMDISK, serial = {serial}, config = {config}")
